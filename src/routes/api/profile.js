bf519d50bfc086d950910825dc46dc0bd49ed363
tomek <tomek@pop-os.localdomain>
2021-08-14 18:39:45 +0200

    create * update profile added

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
@@ -21,5 +22,86 @@ router.get('/me', auth, async (req, res, next) => {
 		res.status(500).send('Service unavailable');
 	}
 });
+
+// @route  POST api/profile/
+// @desc   post create/update a profile
+// @access private
+
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('status', 'Status is required').notEmpty(),
+			check('skills', 'Skills are required').notEmpty(),
+		],
+	],
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			githubusername,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin,
+		} = req.body;
+
+		// build profile object
+
+		const profileFields = {
+			user: req.user.id,
+			social: {},
+		};
+		for (let key in req.body) {
+			if (req.body[key] && key !== 'skills' && !checkSocialMedia(key)) {
+				profileFields[key] = req.body[key];
+			} else if (key === 'skills' && req.body[key]) {
+				const skillsArr = req.body[key].split(',').map((skill) => skill.trim());
+				profileFields[key] = skillsArr;
+			} else if (req.body[key] && checkSocialMedia(key)) {
+				profileFields.social[key] = req.body[key];
+			}
+		}
+		try {
+			let profile = await Profile.findOne({ user: req.user.id });
+			if (profile) {
+				// Update
+				profile = await Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true }
+				);
+				return res.json(profile);
+			}
+			// Create
+			profile = new Profile(profileFields);
+			await profile.save();
+			return res.json(profile);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send('Server malfunction');
+		}
+		res.json(profileFields);
+	}
+);
+
+const checkSocialMedia = (key) => {
+	return (
+		key === 'facebook' ||
+		key === 'twitter' ||
+		key === 'linkedin' ||
+		key === 'instagram'
+	);
+};
 
 module.exports = router;
