208356d49a32fafb23c88c25f85d073ea6cae182
tomek <tomek@pop-os.localdomain>
2021-08-14 14:23:14 +0200

    created authentication

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
 const router = express.Router();
 
 // @route  POST api/users
@@ -16,13 +21,62 @@ router.post(
 			.notEmpty()
 			.isLength({ min: 6, max: 20 }),
 	],
-	(req, res, next) => {
+	async (req, res, next) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		console.log(req.body);
-		res.status(200).json({ message: 'User route...' });
+
+		const { name, email, password } = req.body;
+
+		try {
+			// check if user exists
+			let user = await User.findOne({ email });
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'User already exists' }] });
+			}
+			// get users gravatar
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm',
+			});
+
+			user = new User({
+				email,
+				name,
+				avatar,
+				password,
+			});
+			// encrypt password
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
+			//return jwt
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 36000,
+				},
+				(err, token) => {
+					if (err) {
+						throw err;
+					}
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
