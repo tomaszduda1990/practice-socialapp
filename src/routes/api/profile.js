1daef5963cfb81f5e516995a58ecc20e7eb08435
tomek <tomek@pop-os.localdomain>
2021-08-14 16:47:36 +0200

    get profile api route

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const router = express.Router()
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const router = express.Router();
 
-
-// @route  GET api/profile
+// @route  GET api/profile/me
 // @desc   Test route
-// @access Public 
-router.get('/', (req, res, next) => {
-    res.status(200).json({message: "Profile route..."});
-})
+// @access private
+router.get('/me', auth, async (req, res, next) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).populate(
+			'user',
+			['name', 'avatar']
+		);
+		if (!profile) {
+			return res.status(400).json({ msg: 'There is no profile for this user' });
+		}
+		res.json({ profile });
+	} catch (err) {
+		res.status(500).send('Service unavailable');
+	}
+});
 
 module.exports = router;
