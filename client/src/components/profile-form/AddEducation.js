92f5608286ca7797fd153f6a66c6802ae7228b09
Tomasz Duda <tduda@online.saba.com>
2021-08-23 16:16:52 +0200

    added education experience functios

---
+++ client/src/components/profile-form/AddEducation.js
@@ -0,0 +1,105 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { addEducation } from '../../actions/profile';
+
+const AddEducation = ({ addEducation, history }) => {
+	const [formData, setFormData] = useState({
+		school: '',
+		degree: '',
+		fieldofstudy: '',
+		from: '',
+		to: '',
+		description: '',
+	});
+	const { school, degree, fieldofstudy, from, to, description } = formData;
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		addEducation(formData, history);
+	};
+	return (
+		<section className='container'>
+			<h1 className='large text-primary'>Add education record</h1>
+			<p className='lead'>
+				<i className='fas fa-code-branch'></i> Add any schools that you have
+				been in the past
+			</p>
+			<small>* = required field</small>
+			<form className='form' onSubmit={(e) => onSubmit(e)}>
+				<div className='form-group'>
+					<input
+						type='text'
+						value={school}
+						onChange={(e) => onChange(e)}
+						placeholder='* School'
+						name='school'
+						required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='text'
+						value={degree}
+						onChange={(e) => onChange(e)}
+						placeholder='* Degree'
+						name='degree'
+						required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='text'
+						value={fieldofstudy}
+						onChange={(e) => onChange(e)}
+						placeholder='Field of study'
+						name='fieldofstudy'
+					/>
+				</div>
+				<div className='form-group'>
+					<h4>From Date</h4>
+					<input
+						type='date'
+						name='from'
+						value={from}
+						onChange={(e) => onChange(e)}
+					/>
+				</div>
+				<div className='form-group'>
+					<h4>To Date</h4>
+					<input
+						type='date'
+						name='to'
+						value={to}
+						onChange={(e) => onChange(e)}
+					/>
+				</div>
+				<div className='form-group'>
+					<textarea
+						name='description'
+						cols='30'
+						rows='5'
+						value={description}
+						onChange={(e) => onChange(e)}
+						placeholder='School Description'
+					></textarea>
+				</div>
+				<input type='submit' className='btn btn-primary my-1' />
+				<Link className='btn btn-light my-1' to='/dashboard'>
+					Go Back
+				</Link>
+			</form>
+		</section>
+	);
+};
+
+AddEducation.propTypes = {
+	addEducation: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addEducation })(withRouter(AddEducation));
