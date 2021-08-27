d5143e250607aa2445cbc749a6c459e4640b55ac
Tomasz Duda <tduda@online.saba.com>
2021-08-27 14:33:35 +0200

    removing profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_USER } from './Types';
+import {
+	CLEAR_PROFILE,
+	DELETE_ACCOUNT,
+	GET_PROFILE,
+	PROFILE_ERROR,
+	UPDATE_USER,
+} from './Types';
 import { setAlert } from './alert';
 export const getCurrentUserProfile = () => async (dispatch) => {
 	try {
 		const apiEndpoint = `/api/profile/me`;
 		const res = await axios.get(apiEndpoint);
-		dispatch({ type: GET_PROFILE, payload: res.data });
+		dispatch({ type: GET_PROFILE, payload: res.data.profile });
 	} catch (err) {
 		dispatch({
 			type: PROFILE_ERROR,
@@ -107,5 +113,57 @@ export const addEducation = (formData, history) => async (dispatch) => {
 			type: PROFILE_ERROR,
 			payload: { msg: err.response.statusText, status: err.response.status },
 		});
+	}
+};
+
+export const deleteExperience = (id) => async (dispatch) => {
+	try {
+		const res = await axios.delete(`api/profile/experience/${id}`);
+		dispatch({
+			type: UPDATE_USER,
+			payload: res.data,
+		});
+		dispatch(setAlert('Experience removed', 'success'));
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+export const deleteEducation = (id) => async (dispatch) => {
+	try {
+		const res = await axios.delete(`api/profile/education/${id}`);
+		dispatch({
+			type: UPDATE_USER,
+			payload: res.data,
+		});
+		dispatch(setAlert('Education removed', 'success'));
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};
+
+export const deleteAccount = () => async (dispatch) => {
+	if (window.confirm('Please confirm DELETING your account')) {
+		try {
+			await axios.delete(`/api/profile/delete`);
+			dispatch({
+				type: CLEAR_PROFILE,
+			});
+			dispatch({
+				type: DELETE_ACCOUNT,
+			});
+			dispatch(setAlert('Account succesfully removed'));
+		} catch (err) {
+			dispatch({
+				type: PROFILE_ERROR,
+				payload: { msg: err.response.statusText, status: err.response.status },
+			});
+		}
 	}
 };
