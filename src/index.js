32e7d92b5589aafb67d6183c9adfa824f707a99b
tomek <tomek@pop-os.localdomain>
2021-09-05 22:06:11 +0200

    server corrections

--- src/index.js
+++ src/index.js
@@ -18,13 +18,14 @@ app.use('/api/profile', profileRouter);
 app.use('/api/posts', postsRouter);
 
 // serve static assets  in prod
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'))
-    app.get('/', (req, res, next) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+//     //set static folder
+   
+// }
 
+app.use(express.static(path.join(__dirname, '../', 'client', 'build')))
+app.get('/*', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
